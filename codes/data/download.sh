11796500c97e1cb4985318f904394bf8ac4d4879
myngsooo <hms91018717@gmail.com>
2024-02-28 00:49:50 +0900

    Upload_data

---
+++ codes/data/download.sh
@@ -0,0 +1,3 @@
+wget https://huggingface.co/datasets/THUDM/webglm-qa/resolve/main/data/train.jsonl
+wget https://huggingface.co/datasets/THUDM/webglm-qa/resolve/main/data/validation.jsonl
+wget https://huggingface.co/datasets/THUDM/webglm-qa/resolve/main/data/test.jsonl
