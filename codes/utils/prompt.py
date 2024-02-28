f0a62037fbb7e95aed84d8e22386adf1b2d80a47
Myeong Soo Han <hms91018717@gmail.com>
2024-02-28 12:08:08 +0900

    Update prompt.py

--- codes/utils/prompt.py
+++ codes/utils/prompt.py
@@ -1,14 +1,22 @@
 def get_prompt(mode='test'):
     if mode=='test':
         prompt_template="""
+### System
+You are an AI Assistant. You must give the answer by following Context. While performing the task think step-by-step and justify your steps. Think like you are answering to a five-year-old.
+
 ### Context:
 {context}
 
 ### Question:
 {query}
+
+### Answer
 """
     elif mode=='train':
         prompt_template="""
+### System
+You are an AI Assistant. You must give the answer by following Context. While performing the task think step-by-step and justify your steps. Think like you are answering to a five-year-old.
+
 ### Context:
 {context}
 
