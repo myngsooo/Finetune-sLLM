51fcc347676598d42c84c74aba9792639244f04e
Myeong Soo Han <hms91018717@gmail.com>
2024-02-28 19:30:34 +0900

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -2,7 +2,7 @@ INPUT_FN=''
 CHECK_POINT=''
 
 python ./codes/inference.py \
-    -input_fn ${INPUT_FN} \
+    --input_fn ${INPUT_FN} \
     --checkpoint_dir_path ${CHECK_POINT} \
     --use_4bit \
     --eos_token_id 3 \
